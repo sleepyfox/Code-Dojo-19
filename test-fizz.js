0a20c5c493655fe10553e46e1059619758f1c451
Fox <sleepyfox@gmail.com>
2015-07-14 00:00:34 +0100

    starter files for workshop

---
+++ test-fizz.js
@@ -0,0 +1,13 @@
+should = require('chai').should();
+
+var i, number, rules, should, speak;
+
+fizzbuzz = function(number) {
+  return 0;
+};
+
+describe('fizzbuzz tests', function() {
+  it('fizzbuzz of 1 should be 1', function() {
+    return fizzbuzz(1).should.equal(1);
+  });
+});
